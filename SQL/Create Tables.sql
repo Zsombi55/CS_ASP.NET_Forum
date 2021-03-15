0fc926e989957d4fa1651f78cc93a7bb85217c0e
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-03-15 18:21:58 +0200

    Altered table creation, added UserType insert.
    Swapped FK referencing from UserType and Users _ seems more logical.Added notes in it aobout ideas /questions. Made Insert sql script for UserType table.

--- SQL/Create Tables.sql
+++ SQL/Create Tables.sql
@@ -1,27 +1,26 @@
 --
 -- Database "Forums".
 --
--- basic table aspects:
---
 
+-- Default type is "Regular", only an Admin can promote /demote users.
 CREATE TABLE forums.Users (
 	Id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,
 	Name VARCHAR2(25) NOT NULL UNIQUE ,
 	Pass VARCHAR2(50) NOT NULL ,
 	Email VARCHAR2(255) NOT NULL UNIQUE ,
 	Reg_Date DATETIME NOT NULL ,
-	PRIMARY KEY (Id)
+	UserType_ID INT NOT NULL DEFAULT 1,
+	PRIMARY KEY (Id) ,
+	CONSTRAINT FK_Users_UserType
+		FOREIGN KEY (UserType_ID)
+		REFERENCES UserType (Id)
 ) ENGINE = InnoDB;
 
 -- Admin > Moderator > Regular
 CREATE TABLE forums.UserType (
 	Id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,
 	Name VARCHAR2(25) NOT NULL UNIQUE ,
-	User_ID INT NOT NULL ,
-	PRIMARY KEY (Id) ,
-	CONSTRAINT FK_UserType_Users
-		FOREIGN KEY (User_ID)
-		REFERENCES Users (Id)
+	PRIMARY KEY (Id)
 ) ENGINE = InnoDB;
 
 -- Forum / Board / Category
@@ -29,8 +28,10 @@ CREATE TABLE forums.Boards (
 	Id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,
 	Name VARCHAR2(255) NOT NULL UNIQUE ,
 	Description VARCHAR2(255) NOT NULL UNIQUE ,
-	PRIMARY KEY (Id)
-) ENGINE = InnoDB;
+	Reg_By_Admin_User_ID INT NOT NULL ,
+	PRIMARY KEY (Id) ,
+	CONSTRAINT FK_Boards_Admin_Users FOREIGN KEY (Reg_By_Admin_User_ID) REFERENCES Users (Id)
+) ENGINE = InnoDB; -- ONLY "Admin" TYPE Users CAN BE HERE !  How to reference with a constraint like that ?
 
 -- Thread / Topic
 CREATE TABLE forums.Threads (
@@ -41,19 +42,19 @@ CREATE TABLE forums.Threads (
 	Reg_By_User_ID INT NOT NULL ,
 	PRIMARY KEY (Id) ,
 	CONSTRAINT FK_Threads_Boards FOREIGN KEY (Board_ID) REFERENCES Boards (Id) ,
-	CONSTRAINT FK_UserType_Users FOREIGN KEY (Reg_By_User_ID) REFERENCES Users (Id)
-) ENGINE = InnoDB;
+	CONSTRAINT FK_Threads_Users FOREIGN KEY (Reg_By_User_ID) REFERENCES Users (Id)
+) ENGINE = InnoDB; -- Limit "Thread" count: no reason to store more than 1.000 posts per Thread; make new Thread instead.
 
 CREATE TABLE forums.Posts (
 	Id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,
-	Content NVARCHAR2(255) NOT NULL UNIQUE ,
+	Content VARCHAR(60000) NOT NULL ,
 	Reg_Date DATETIME NOT NULL ,
-	Thread_ID INT NOT NULL ,
+	Thread_ID INT NOT NULL UNIQUE ,
 	Reg_By_User_ID INT NOT NULL ,
 	PRIMARY KEY (Id) ,
 	CONSTRAINT FK_UserType_Users FOREIGN KEY (Thread_ID) REFERENCES Threads (Id) ,
-	CONSTRAINT FK_UserType_Users FOREIGN KEY (Reg_By_User_ID) REFERENCES Users (Id)
-) ENGINE = InnoDB;
+	CONSTRAINT FK_Posts_Users FOREIGN KEY (Reg_By_User_ID) REFERENCES Users (Id)
+) ENGINE = InnoDB; -- "Content" size: if users want to post book volumes, let them.
 
 ------------
 -- Forums --
---
+++ SQL/Insert_UserType.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO forums.UserType (Id, Name) VALUES
+(1, N'Regular') ,
+(2, N'Moderator') ,
+(3, N'Admin');

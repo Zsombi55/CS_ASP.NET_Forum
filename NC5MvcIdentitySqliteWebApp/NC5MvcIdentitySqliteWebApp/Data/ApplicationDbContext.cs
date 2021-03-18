c1ee50af687052a3235f0a6b0d3989a05a94f2c2
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-03-18 14:51:42 +0200

    Initial code: .Net Core 5, MVC, Identity, SQLite.

---
+++ NC5MvcIdentitySqliteWebApp/NC5MvcIdentitySqliteWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NC5MvcIdentitySqliteWebApp.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}

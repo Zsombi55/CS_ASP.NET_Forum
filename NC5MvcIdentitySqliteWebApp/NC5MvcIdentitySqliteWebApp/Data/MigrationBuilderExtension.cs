6db6d00220c7d6a2c9fba8877d10b049ef6c9a66
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-03-25 12:15:27 +0200

    Possibly found SQLite syntax for adding users with roles if password was successfully hashed. Will see if it actually woks after it is made for the other actions too.

--- NC5MvcIdentitySqliteWebApp/NC5MvcIdentitySqliteWebApp/Data/MigrationBuilderExtension.cs
+++ NC5MvcIdentitySqliteWebApp/NC5MvcIdentitySqliteWebApp/Data/MigrationBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,14 +18,14 @@ namespace NC5MvcIdentitySqliteWebApp.Data
 		/// <remarks>Best not to hardcode the SQL code, syntax differs, eg.: SQLServer != SQLite.</remarks>
 		public static void AddRole(this MigrationBuilder migration, string roleName)
 		{
-			if(string.IsNullOrWhiteSpace(roleName))
+			if (string.IsNullOrWhiteSpace(roleName))
 			{
 				return;
 			}
 
 			string normalizedRoleName = roleName.ToUpperInvariant();
 			//migration.ActiveProvider -- check msdn
-			// SQLite syntax:
+			// SQLite syntax (if there's no INT AUTOINCREMENT, the hidden ROWID is automatically used as such):
 			migration.Sql($@"
 				INSERT INTO AspNetRoles (Name, NormalizedName, ConcurrencyStamp)
 				SELECT '{roleName}', '{normalizedRoleName}', '{Guid.NewGuid().ToString()}'
@@ -40,6 +41,71 @@ namespace NC5MvcIdentitySqliteWebApp.Data
 			//		VALUES (NEWID(), '{roleName}', '{normalizedRoleName}', '{Guid.NewGuid().ToString()}')
 			//	END;
 			//");
+		}
+
+		public static void AddUserWithRoles(this MigrationBuilder migrationBuilder,
+			string email, string password, string[] roleNames)
+		{
+			if(string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+			{ return; }
+
+			string normalizedEmail = email.ToUpperInvariant();
+			// TODO: Add "UserName" and "normalizedUserName" too; they MUST NOT be the same as Email !!
+			var identityUser = new IdentityUser
+			{
+				UserName = email,
+				NormalizedUserName = normalizedEmail,
+				Email = email,
+				NormalizedEmail = normalizedEmail,
+				EmailConfirmed = true,
+				SecurityStamp = Guid.NewGuid().ToString(),
+				ConcurrencyStamp = Guid.NewGuid().ToString(),
+				PhoneNumberConfirmed = false,
+				TwoFactorEnabled = false,
+				LockoutEnabled = false,
+				AccessFailedCount = 0
+			};
+
+			var options = Microsoft.Extensions.Options.Options.Create<PasswordHasherOptions>(
+				new PasswordHasherOptions
+				{
+					CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV3,
+					IterationCount = 10000
+				}
+			);
+
+			var passwordHasher = new PasswordHasher<IdentityUser>(options);
+			string passwordHash = passwordHasher.HashPassword(identityUser, password);
+
+			if(passwordHasher.VerifyHashedPassword(identityUser, passwordHash, password) ==
+				PasswordVerificationResult.Failed)
+			{
+				throw new Exception("Unable to correctly generate password hash!");
+			}
+
+			// ---IF ALL WENT WELL, BEGIN NEW USER INSERTION---
+			// SQLite: No need to insert "Id": https://www.sqlitetutorial.net/sqlite-autoincrement/
+			migrationBuilder.Sql($@"
+				INSERT INTO AspNetUsers (UserName, NormalizedUserName, Email, NormalizedEmail, 
+					EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp, 
+					PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount)
+				SELECT 
+					'{email}',							/* UserName */
+					'{normalizedEmail}', 				/* NormailzedEmail */
+					'{email}', 							/* Email */
+					'{normalizedEmail}', 				/* NormailzedEmail */
+					1, 									/* EmailConfirmed */
+					'{passwordHash}', 					/* PasswordHash */
+					'{identityUser.SecurityStamp}', 	/* SecurityStamp */
+					'{identityUser.ConcurrencyStamp}', 	/* ConcurrencyStamp */
+					0, 									/* PhoneNumberConfirmed */
+					0, 									/* TwoFactorEnabled */
+					0, 									/* LockoutEnabled */
+					0, 									/* AccessFailedCount */
+				WHERE NOT EXISTS (
+					SELECT * FROM AspNetUsers 
+					WHERE NormalizedUserName = '{normalizedEmail}' OR NormalizedEmail = '{normalizedEmail}');
+			");
 		}
 	}
 }

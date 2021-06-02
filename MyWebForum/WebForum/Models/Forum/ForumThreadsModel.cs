ffdc79f142a3ba4ce4ba86fcfaaa2fcbc363a5e6
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-06-02 18:17:05 +0300

    Forum.Threads view rework, adding thread search.

--- MyWebForum/WebForum/Models/Forum/ForumThreadsModel.cs
+++ MyWebForum/WebForum/Models/Forum/ForumThreadsModel.cs
@@ -4,12 +4,15 @@ using WebForum.Models.Thread;
 namespace WebForum.Models.Forum
 {
 	/// <summary>
-	/// View Model: a Forum and its Thread collection.
+	/// View Model: a Forum, its Thread collection, and relevant search query string.
 	/// </summary>
 	public class ForumThreadsModel
 	{
 		public ForumListingModel Forum { get; set; }
 
 		public IEnumerable<ThreadListingModel> Threads { get; set; }
+
+
+		public string SearchQuery { get; set; }
 	}
 }

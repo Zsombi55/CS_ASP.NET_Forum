98ac4bde723307d88851af91b954dca054e346db
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2021-07-03 19:23:47 +0300

    Db migration, update. Scaffolded Identity > Register for later editing.

--- MyWebForum/WebForum/Migrations/20210703155549_PreIdentityRegistrationScaffolding.Designer.cs
+++ MyWebForum/WebForum/Migrations/20210703155549_PreIdentityRegistrationScaffolding.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebForum.Data;
 
 namespace WebForum.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210703155549_PreIdentityRegistrationScaffolding")]
+    partial class PreIdentityRegistrationScaffolding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
